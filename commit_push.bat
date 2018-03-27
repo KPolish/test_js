dfc83bd264a4d017b5bc7bbd48f0b88523865e0a
KPolish <com94ok@naver.com>
2018-03-27 12:51:19 +0900

    20180327 tset.js#4

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180327 tset.js#3"
+git commit -m "20180327 tset.js#4"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,47 @@
+function getIdPw() {
+    var id = prompt('ID입력', '');
+    var password = prompt(id + '의 사용할 PW입력', '');
+    localStorage.setItem('id', id);
+    localStorage.setItem('password', password);
+}
+var password = localStorage.getItem('password');
+//if (!password) {
+//    getIdPw();
+//}
+getIdPw();
+//password 비교부분 추가해야함
+function setIdPw() {
+    var pww = prompt('PW 입력','');
+    if (pww === localStorage.getItem('password'))
+    alert('정상 로그인!');
+    else alert('비밀번호가 틀립니다.');
+}
+
+var myButton = document.querySelector('button');
+myButton.onclick = function () {
+    setIdPw();
+}
+
+var id = localStorage.getItem('id');
+var heading = document.querySelector('h1');
+heading.innerHTML = id + 'Homepage';
+var myImg = document.querySelector('img');
+myImg.onclick = function() {
+    var src = myImg.getAttribute('src');
+    if (src === 'images/firefox-icon.png')
+        myImg.setAttribute('src', 'images/github.png');
+    else
+        myImg.setAttribute('src', 'images/firefox-icon.png');
+}
+
+
+
+
+
+
+
+
+/*
 var myHeading = document.querySelector('h1');
 var id = localStorage.getItem('id');
 myHeading.innerHTML = 'Cloud Service<br>20180327<br>' + id;
@@ -18,10 +62,10 @@ function getIdPw() {
     else alert('비번오류');
 }
 //getIdPw();
-/*
-document.querySelector('h1').onclick = function() {
-    getIdPw();
-} */
+
+//document.querySelector('h1').onclick = function() {
+//    getIdPw();
+//} 
 var myImg = document.querySelector('img');
 myImg.onclick = function() {
     var src = myImg.getAttribute('src');
@@ -34,4 +78,4 @@ myImg.onclick = function() {
 var myButton = document.querySelector('button');
 myButton.onclick = function () {
     getIdPw();
-}
+}*/
