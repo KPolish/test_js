8d3201afdcb81e049aa6387fcedbbe30dbd6e19d
KPolish <com94ok@naver.com>
2018-03-27 11:14:05 +0900

    20180327 tset.js#2

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "20180327 tset.js#1"
+git commit -m "20180327 tset.js#2"
 git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,8 @@ var b = a*text;
 var iceCream = 'chocolate';
 var id = prompt('ID 입력','');
 alert(id+'가 로그인함.');
+var passwordSys = '12345';
+var password = prompt('PW 입력','');
+if (password === passwordSys)
+alert('정상 로그인!');
+else alert('비번오류');
