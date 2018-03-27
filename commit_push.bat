b7a2c96f20db5fa6017a20b8ec2a2d4dbc6359ee
KPolish <com94ok@naver.com>
2018-03-27 10:45:46 +0900

    20180327 tset.js#1

---
+++ commit_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "20180327 tset.js#1"
+git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Cloud Service';
+myHeading.innerHTML = 'Cloud Service<br>20180327';
 var text = 'This is a text.';
-alert(text);
+text=10.5;
+var a = 33.7;
+var b = a*text;
+//alert(text);
+var iceCream = 'chocolate';
+var id = prompt('ID 입력','');
+alert(id+'가 로그인함.');
